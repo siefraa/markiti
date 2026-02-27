6178c83991f8f81adca0bf6f4af3331e9e5c7357
Mercurial <oo@Mercurial.local>
2026-02-27 16:17:12 +0300

    Fix Kotlin DSL: versionCode & versionName

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,19 @@ plugins {
 }
 
 android {
-    namespace = "com.example.twende_markiti"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.markiti"
+        minSdk = 21
+        targetSdk = 34
+
+        // Use function calls
+        versionCode(flutter.versionCode)
+        versionName(flutter.versionName)
+
+        multiDexEnabled = true
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -16,26 +26,9 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.twende_markiti"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+        // Use compilerOptions DSL instead of deprecated jvmTarget string
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+        jvmTarget = "17"
     }
 }
 
